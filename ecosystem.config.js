a8c99e700cf30ebe4348764037feeea2c35eaa10
Mike Kalinin <mike.kalinin@spryker.com>
2021-01-15 01:04:38 +0200

    Adjusting Java memory for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,10 @@ module.exports = {
             script: "./public/index.js",
             autorestart: true,
             env: {
-                "PORT": "80",
+                PORT: "80",
+                JAVA_OPTS: (process.env.JAVA_OPTS || '')
+                + ` -Xms4096m`        //set initial Java heap size
+                + ` -Xmx8192m`        //set maximum Java heap size
             }
         }
     ]
